20be21867e66d40d4f384c2b9adcdf38ab664c89
jmartinez <jyela@playtechla.com>
2025-08-04 12:56:41 -0500

    Prueba usuario

--- ShopServer/database/mongo.js
+++ ShopServer/database/mongo.js
@@ -2,6 +2,7 @@ const { connect, set } = require('mongoose');
 
 const DB_URI = `mongodb+srv://jhonier:13052002@restaurant.hluhpeu.mongodb.net/workshop?retryWrites=true&w=majority&appName=workshop`;
 
+
 const dbInit = async () => {
     set("strictQuery", false);
     await connect(`${DB_URI}`);
