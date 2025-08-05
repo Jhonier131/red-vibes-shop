abd721864dd9d55d8b46168d37869a0b8bb6aa1c
jmartinez <96468674+Jhonier131@users.noreply.github.com>
2025-08-05 08:43:16 -0500

    prueba commit

--- ShopServer/database/mongo.js
+++ ShopServer/database/mongo.js
@@ -3,6 +3,7 @@ const { connect, set } = require('mongoose');
 const DB_URI = `mongodb+srv://jhonier:13052002@restaurant.hluhpeu.mongodb.net/workshop?retryWrites=true&w=majority&appName=workshop`;
 
 
+console.log("Holaaa");
 const dbInit = async () => {
     set("strictQuery", false);
     await connect(`${DB_URI}`);
